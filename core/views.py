4e5b3ea42f9d052566c932e55b8a50101eb4e797
divyanshikathuria <divyanshikathuria@gmail.com>
2015-06-26 17:51:33 +0530

    SignUp Module

--- core/views.py
+++ core/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from permissions import IsAuthenticatedOrCreate
 # Create your views here.
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from core.serializers import UserSerializer, GroupSerializer
-
+from core.serializers import UserSerializer, GroupSerializer,SignUpSerializer
+from rest_framework import generics
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -21,3 +22,21 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class SignUp(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
+    permission_classes = (IsAuthenticatedOrCreate,)
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            User.objects.create_user(**serializer.validated_data)
+
+            return Response(serializer.validated_data)
+
+        return Response({
+            
+            'message': 'Account could not be created with received data.'
+        })
+
